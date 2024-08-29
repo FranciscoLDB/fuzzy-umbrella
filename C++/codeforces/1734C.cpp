51d51f7537538acc78e12350b8708bd9159610f8
Francisco Luis <83092448+FranciscoLDB@users.noreply.github.com>
2024-08-29 20:05:36 +0000

    1734C trying

--- C++/codeforces/1734C.cpp
+++ C++/codeforces/1734C.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 
+
+
 int main(){
     int t = 0;
     std::cin >> t;
     int resp[t] = {};
     for(int i = 0; i < t; i++){
+        
         long n = 0;
         std::cin >> n;
-        int binary[n] = {};
-        for(long j = 0; j < n; j++){
-            int b = 0;
-            std::cin >> b;
-            binary[j] = b;
-        }
+        int b = 0;
+        std::cin >> b;
 
+        int T[n] = {};
+        int cont0 = 0;
+        int* E = nullptr;
+        for(long j = 0; j < n; j++){
+            T[j] = b;
+            if(!b) cont0++;
+        }
+        E = new int[cont0];
+        for(long j = 0; j < n; j++){
+            std::cout << T[j];
+        }
+        std::cout << std::endl;
     }
     for(int i = 0; i < t; i++){
         std::cout << resp[i] << std::endl;
